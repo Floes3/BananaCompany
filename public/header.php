d13615eaa116a982f320b4d4ee6725eaa477bb8d
DavidvdBurgh <davidvdburgh@live.nl>
2015-10-21 12:36:07 +0200

    header.php en main.css
    header.php aangepast en main.css aangevuld met main-david.css

--- public/header.php
+++ public/header.php
@@ -12,7 +12,7 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <link rel="stylesheet" href="<?php echo HTTP . 'public/assets/css/normalize.min.css'?>">
         <link rel="stylesheet" href="<?php echo HTTP . 'public/assets/css/main.css'?>">
-        <link rel="stylesheet" href="<?php echo PUBLIC_FOLDER . 'assets/css/main-david.css'?>">
+        <link rel="stylesheet" href="<?php echo HTTP . 'public/assets/css/main-david.css'?>">
         <link rel="stylesheet" href="http://bootswatch.com/flatly/bootstrap.min.css"/>
 
         <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
@@ -20,6 +20,5 @@
         <script src="<?php echo PUBLIC_FOLDER . 'assets/js/vendor/modernizr-2.8.3.min.js'?>"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     </head>
-   
+
     <body>
-    
