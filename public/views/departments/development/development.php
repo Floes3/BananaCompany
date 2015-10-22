18a9c96ded6b067d6e2977831c42b3f553f23ecc
Sjoerd <sjoerd_3@live.nl>
2015-10-22 11:15:43 +0200

    Header & development

--- public/views/departments/development/development.php
+++ public/views/departments/development/development.php
@@ -1,1 +1,49 @@
-hi development!
+<?php
+
+require_once '../../../header.php';
+ ?>
+
+<header>
+    <div class="col-md-12">
+        <h1>barroc IT</h1>
+    </div>
+</header>
+<div id='cssmenu'>
+    <ul>
+        <li class='active'><a href='#'>Home</a></li>
+        <li><a href='#'>Products</a></li>
+        <li><a href='#'>Company</a></li>
+        <li><a href='#'>Contact</a></li>
+        <form action="../../../../app/controllers/authController.php" method="POST">
+            <li><input type="submit" value="Logout"></li>
+            <input type="hidden" name="type" value="logout">
+        </form>
+        
+    </ul>
+</div>
+<?php
+        if($messageBag->hasMsg()){
+           echo $messageBag->show();
+        }
+    ?>
+<div class="container">
+    
+    <div class="col-md-10 dash-title">
+        <h1>Dashboard | Development</h1>
+    </div>
+    
+    <div class="table">
+        <div class="col-md-6">
+            <h2>Appointments</h2>
+            <a href=""><p>Add,view,delete or edit appointments</p></a>
+        </div>
+        <div class="col-md-6">
+            <h2>Client information</h2>
+            <a href=""><p>Add,view,delete or edit client</p></a>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
