64e3e7bc10795283e71f37f9ef596ba1d9c4ea4d
Rhobin <balentien@gmail.com>
2015-10-23 09:35:21 +0200

    table toegevoegd + eigen css file aangemaakt

--- public/views/departments/finance/finance.php
+++ public/views/departments/finance/finance.php
@@ -1,1 +1,90 @@
-hi finance!
+<?php
+require_once '../../../../app/init.php';
+require_once '../../../header.php';
+ ?>
+
+<header>
+    <div class="col-md-12">
+        <h1>barroc IT</h1>
+    </div>
+</header>
+<div id='cssmenu'>
+    <ul>
+        <li class='active'><a href='#'>Home</a></li>
+        <li><a href='#'>Products</a></li>
+        <li><a href='#'>Company</a></li>
+        <li><a href='#'>Contact</a></li>
+    </ul>
+</div>
+<div class="container">
+    <div class="col-md-10 dash-title">
+        <h1>Dashboard</h1>
+    </div>
+    <div class="table">
+        <div class="col-md-6">
+            <h2>Invoices</h2>
+            <a href=""><p>view</p></a>
+            <table class="table table-bordered">
+	            <thead>
+			      <tr>
+			        <th>Firstname</th>
+			        <th>Lastname</th>
+			        <th>Email</th>
+			      </tr>
+			    </thead>
+			    <tbody>
+			      <tr>
+			        <td>John</td>
+			        <td>Doe</td>
+			        <td>john@example.com</td>
+			      </tr>
+			      <tr>
+			        <td>Mary</td>
+			        <td>Moe</td>
+			        <td>mary@example.com</td>
+			      </tr>
+			      <tr>
+			        <td>July</td>
+			        <td>Dooley</td>
+			        <td>july@example.com</td>
+			      </tr>
+			    </tbody>
+			</table>
+        </div>
+        <div class="col-md-6">
+            <h2>Client information</h2>
+            <a href=""><p>view</p></a>
+            <table class="table table-bordered">
+	            <thead>
+			      <tr>
+			        <th>Firstname</th>
+			        <th>Lastname</th>
+			        <th>Email</th>
+			      </tr>
+			    </thead>
+			    <tbody>
+			      <tr>
+			        <td>John</td>
+			        <td>Doe</td>
+			        <td>john@example.com</td>
+			      </tr>
+			      <tr>
+			        <td>Mary</td>
+			        <td>Moe</td>
+			        <td>mary@example.com</td>
+			      </tr>
+			      <tr>
+			        <td>July</td>
+			        <td>Dooley</td>
+			        <td>july@example.com</td>
+			      </tr>
+			    </tbody>
+			</table>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
+
